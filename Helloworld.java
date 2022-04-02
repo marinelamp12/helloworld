8d43b563a1651be850b9b1b03c77afc3105eb69e
marinelamp12 <102840536+marinelamp12@users.noreply.github.com>
2022-04-02 11:50:49 +0900

    파일 업로드!!

---
+++ Helloworld.java
@@ -0,0 +1,25 @@
+//package ex0l 프로그램을 모아놓은 펄더
+public class Helloworld //
+{
+
+	public static void main(String[] args) //Helloworld.java 불러오기 
+	{	//main 함수는 가장먼저 실행됨
+		//void함수는 return값이 없는 함수를 의미함.
+		// "//"는 한 줄 주석
+		// "/* ~ */"는 여러줄 주석
+		System.out.print("Hellowold!!");//print는 줄바꿈 없는 출력
+		System.out.print("Hellowold!!\t");// \t는 tab
+		System.out.print("Hellowold!!\n");// \n은 줄바꿈
+		System.out.println("Hellowold!!\\n");// \는 뒤의 문자를 무조건 기호 취급하여 기능을 없앰
+		System.out.println("Hellowold!!");//println은 줄바꿈
+		
+		
+		System.out.println("7");
+		System.out.println(3.15);
+		System.out.println("3" + 4 + "55" + 2);
+		System.out.println("3" + (4 + 2) + "55");
+		System.out.println("3" + 4 + 2 + "55");
+		System.out.println( 4 + 2 + "55");
+	}
+
+}
